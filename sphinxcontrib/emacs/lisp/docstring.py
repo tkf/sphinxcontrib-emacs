9ce18b2e146b0d486ae30c86e486f780d28ea6b5
Sebastian Wiesner <lunaryorn@gmail.com>
2014-02-28 10:57:10 +0100

    Parse definition lists in docstrings

--- sphinxcontrib/emacs/lisp/docstring.py
+++ sphinxcontrib/emacs/lisp/docstring.py
@@ -36,21 +36,35 @@ from sphinxcontrib.emacs.nodes import el_metavariable
 
 class Parser(object):
     def __init__(self, reporter):
-        self.state_classes = [Body, Text]
         self.reporter = reporter
 
     def parse(self, inputstring):
-        statemachine = StateMachineWS(state_classes=self.state_classes,
-                                      initial_state='Body',
-                                      debug=True)
-        statemachine.reporter = self.reporter
-        statemachine.inliner = Inliner()
+        statemachine = DocstringStateMachine(
+            initial_state='Body', reporter=self.reporter, debug=True)
         inputlines = string2lines(inputstring, tab_width=8,
                                   convert_whitespace=True)
         return statemachine.run(inputlines)
 
 
-class Body(StateWS):
+class DocstringStateMachine(StateMachineWS):
+
+    def __init__(self, initial_state, reporter, debug=False, inliner=None):
+        StateMachineWS.__init__(self, state_classes=STATE_CLASSES,
+                                initial_state=initial_state, debug=debug)
+        self.reporter = reporter
+        self.inliner = inliner or Inliner()
+
+    def make_nested(self, inital_state):
+        return self.__class__(
+            initial_state=inital_state, reporter=self.reporter,
+            debug=self.debug, inliner=self.inliner)
+
+
+class DocstringState(StateWS):
+    pass
+
+
+class Body(DocstringState):
     patterns = {'text': r''}
     initial_transitions = ('text',)
 
@@ -58,11 +72,48 @@ class Body(StateWS):
         return [match.string], 'Text', []
 
 
-class Text(StateWS):
+class SpecializedBody(Body):
+
+    # Abort all body states, to let subclasses only enable the specific states
+    # allowed in a special body part
+
+    def _invalid_input(*args):
+        self.state_machine.previous_line()
+        raise EOFError()
+
+    text = _invalid_input
+
+
+class DefinitionList(SpecializedBody):
+    def text(self, match, context, next_state):
+        # Instead of parsing plain text as in Body, we now proceed to parse a
+        # Definition
+        return [match.string], 'Definition', []
+
+
+class Text(DocstringState):
     patterns = {'text': r''}
     initial_transitions = [('text', 'Body')]
 
-    def paragraph(self, lines, lineno=None):
+    # Helpers
+
+    def _nested_parse(self, block, input_offset):
+        state_machine = self.state_machine.make_nested('Body')
+        children = state_machine.run(block)
+        return children, state_machine.abs_line_offset()
+
+    def _nested_list_parse(self, lines, input_offset, initial_state,
+                           blank_finish, blank_finish_state=None):
+        state_machine = self.state_machine.make_nested(initial_state)
+        nodes = state_machine.run(lines, input_offset)
+        if blank_finish_state is None:
+            blank_finish_state = initial_state
+        state_machine.states[blank_finish_state].blank_finish = blank_finish
+        blank_finish = state_machine.states[blank_finish_state].blank_finish
+        state_machine.unlink()
+        return nodes, state_machine.abs_line_offset(), blank_finish
+
+    def _paragraph(self, lines, lineno=None):
         if lineno is None:
             lineno = self.state_machine.abs_line_number() - 1
         text = '\n'.join(lines).rstrip()
@@ -72,12 +123,58 @@ class Text(StateWS):
         para.source, para.line = self.state_machine.get_source_and_line(lineno)
         return para
 
+    def _make_definition_list_item(self, termlines):
+        assert len(termlines) == 1
+        # Get the indented block
+        indented, _, line_offset, blank_finish = self.state_machine.get_indented()
+        item = nodes.definition_list_item(
+            '\n'.join(termlines + list(indented)))
+        # Compensate for the term line which was already parsed
+        lineno = self.state_machine.abs_line_number() - 1
+        item.source, item.line = self.state_machine.get_source_and_line(lineno)
+        # Parse the term line
+        term = nodes.term(termlines[0])
+        term.extend(self.state_machine.inliner.parse(termlines[0]))
+        term.source, term.line = self.state_machine.get_source_and_line(lineno)
+        item += term
+        # And the definition
+        definition = nodes.definition('')
+        item += definition
+        children, _ = self._nested_parse(indented, input_offset=line_offset)
+        definition.extend(children)
+        return item, blank_finish
+
+    # Whitespace transitions
+
     def eof(self, context):
-        return [self.paragraph(context)]
+        return [self._paragraph(context)]
+
+    def indent(self, match, context, next_state):
+        definition_list = nodes.definition_list()
+        item, blank_finish = self._make_definition_list_item(context)
+        definition_list += item
+        offset = self.state_machine.line_offset + 1   # next line
+        children, newline_offset, blank_finish = self._nested_list_parse(
+            self.state_machine.input_lines[offset:],
+            input_offset=self.state_machine.abs_line_offset() + 1,
+            initial_state='DefinitionList',
+            blank_finish=blank_finish, blank_finish_state='Definition',)
+        definition_list.extend(children)
+        self.state_machine.goto_line(newline_offset)
+        result = [definition_list]
+        if not blank_finish:
+            lineno = self.state_machine.abs_line_number() + 1
+            reporter = self.state_machine.reporter
+            result.append(
+                reporter.warning('Definition list ends without a blank line; '
+                                 'unexpected unindent.', line=lineno))
+        return [], 'Body', result
 
     def blank(self, match, context, next_state):
         """End of a paragraph"""
-        return [], 'Body', [self.paragraph(context)]
+        return [], 'Body', [self._paragraph(context)]
+
+    #  State transitions
 
     def text(self, _match, context, next_state):
         msg = None
@@ -88,11 +185,50 @@ class Text(StateWS):
             msg = self.state_machine.reporter.error(
                 'Unexpected indentation.', source=src, line=srcline)
         lines = context + list(block)
-        paragraph = self.paragraph(lines)
+        paragraph = self._paragraph(lines)
         result = [paragraph]
         if msg is not None:
             result.append(msg)
         return [], next_state, result
+
+
+class SpecializedText(Text):
+
+    # Same pattern as in SpecializedBody.  We disable all state transitions, so
+    # that subclasses can re-enable and override the permitted ones.
+
+    def eof(self, _context):
+        # The current construct is incomplete
+        return []
+
+    def _invalid_input(self, _match=None, _context=None, _next_state=None):
+        raise EOFError
+
+    text = _invalid_input
+    blank = _invalid_input
+    indent = _invalid_input
+
+
+class Definition(SpecializedText):
+    def eof(self, _context):
+        # If we are at EOF, when we should be in a definition, we are obviously
+        # wrong, so retract and let the parent state machine reassess the
+        # situation
+        self.state_machine.previous_line(2)
+        return []
+
+    def indent(self, _match, context, _next_state):
+        # Parse the current definition list item
+        item, blank_finish = self._make_definition_list_item(context)
+        self.blank_finish = blank_finish
+        return [], 'DefinitionList', [item]
+
+
+STATE_CLASSES = [
+    Body, Text,                 # Basic states
+    DefinitionList, Definition, # Definition lists
+]
+
 
 class Inliner(object):
 
@@ -151,6 +287,7 @@ class Inliner(object):
         return [nodes.Text(groups['urlprefix'], groups['urlprefix']), ref]
 
     def handle_symbol_reference(self, rawtext, value, _groups):
+        # FIXME: Parse nested literals!
         return self._make_reference(rawtext, value, ('el', 'symbol'))
 
     def handle_metavar(self, rawtext, value, _groups):
